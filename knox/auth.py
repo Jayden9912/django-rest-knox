f99a754406f21a1374546121388cc40def5073e5
Jayden9912 <85933053+Jayden9912@users.noreply.github.com>
2022-05-24 00:13:24 +0800

    debug

--- knox/auth.py
+++ knox/auth.py
@@ -83,14 +83,11 @@ class TokenAuthentication(BaseAuthentication):
 
     def renew_token(self, auth_token):
         current_expiry = auth_token.expiry
-        print(current_expiry)
-        new_expiry = timezone.now() + knox_settings.TOKEN_TTL
-        print(timezone.now())
-        token_TL = current_expiry - timezone.now()
-        print(token_TL)
-        print(token_TL.total_seconds())
+        current_time = timezone.now()
+        new_expiry = current_time + knox_settings.TOKEN_TTL
+        token_TL = (current_expiry - current_time).total_seconds()
         delta = (new_expiry - current_expiry).total_seconds()
-        if delta < knox_settings.MIN_TOKEN_TL:
+        if token_TL < knox_settings.MIN_TOKEN_TL:
             auth_token.expiry = new_expiry
         # Throttle refreshing of token to avoid db writes
         if delta > knox_settings.MIN_REFRESH_INTERVAL:
